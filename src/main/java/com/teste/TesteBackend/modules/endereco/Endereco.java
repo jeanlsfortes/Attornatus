938906e6fda7b951dd03bf5b668ee0666d2de6b3
Jean Lucas Solano Fortes <jeanlucaseh10@gmail.com>
2023-01-09 16:38:47 -0400

    iniciando mapeamento do banco

--- src/main/java/com/teste/TesteBackend/modules/endereco/Endereco.java
+++ src/main/java/com/teste/TesteBackend/modules/endereco/Endereco.java
@@ -1,11 +1,26 @@
 package com.teste.TesteBackend.modules.endereco;
 
 import com.teste.TesteBackend.modules.endereco.enums.TipoEnderco;
+import com.teste.TesteBackend.modules.pessoa.Pessoa;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Table(name = "endereco")
 public class Endereco {
 
+    @Id
     private Long id;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_endereco")
     private TipoEnderco tipo;
     
     private String lagouro;
@@ -15,4 +30,7 @@ public class Endereco {
     private Long numero;
 
     private String cidade;
+
+    @JoinColumn(name = "id_pessoa", foreignKey = @ForeignKey(name= "pessoa_endereco"))
+    private Pessoa pessoa;
 }
--- src/main/java/com/teste/TesteBackend/modules/pessoa/Pessoa.java
+++ src/main/java/com/teste/TesteBackend/modules/pessoa/Pessoa.java
@@ -5,18 +5,27 @@ import java.util.List;
 
 import com.teste.TesteBackend.modules.endereco.Endereco;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.Data;
 
+@Data
+@Table(name = "pessoa")
 public class Pessoa {
     
+    @Id
     private String Nome;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "data_nascimento", nullable = false, columnDefinition = "DATE")
     private Date dataNascimento;
 
+    @OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL)
     private List<Endereco> enderecos;
 
 }
