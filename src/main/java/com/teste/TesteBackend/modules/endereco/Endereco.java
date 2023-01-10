8296e53a3cd736d216bddbfdfc82b85b50194cc7
Jean Lucas Solano Fortes <jeanlucaseh10@gmail.com>
2023-01-10 16:26:38 -0400

    povoando o BD

--- src/main/java/com/teste/TesteBackend/modules/endereco/Endereco.java
+++ src/main/java/com/teste/TesteBackend/modules/endereco/Endereco.java
@@ -1,36 +1,53 @@
 package com.teste.TesteBackend.modules.endereco;
 
 import com.teste.TesteBackend.modules.endereco.enums.TipoEnderco;
+import com.teste.TesteBackend.modules.pessoa.Pessoa;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
-@Table(name = "endereco")
 @Entity
+@Table(name = "endereco")
 public class Endereco {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "tipo_endereco")
     private TipoEnderco tipo;
-    
-    private String lagouro;
 
+    @ManyToOne
+    @JoinColumn(name = "id_pessoa", foreignKey = @ForeignKey(name="fk_pessoa_endereço"))
+    private Pessoa pessoa;
+
+    @Column(name = "logradouro")
+    private String logradouro;
+
+    @Column(name = "cep")
     private String cep;
 
+    @Column(name = "numero")
     private Long numero;
 
+    @Column(name = "cidade")
     private String cidade;
 
 }
--- src/main/java/com/teste/TesteBackend/modules/pessoa/Pessoa.java
+++ src/main/java/com/teste/TesteBackend/modules/pessoa/Pessoa.java
@@ -3,10 +3,14 @@ package com.teste.TesteBackend.modules.pessoa;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.teste.TesteBackend.modules.endereco.Endereco;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,21 +18,30 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 @Table(name = "pessoa")
 public class Pessoa {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nome")
     private String Nome;
 
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "data_nascimento", nullable = false, columnDefinition = "DATE")
     private Date dataNascimento;
 
-    @OneToMany(mappedBy = "pessoa", cascade = CascadeType.ALL)
+    @JsonIgnore
+    @OneToMany(mappedBy = "pessoa", fetch = FetchType.LAZY)
     private List<Endereco> enderecos;
 
 }
